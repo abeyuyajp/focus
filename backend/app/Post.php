ef33477086d27114f50f224a2cec1c5ad225e602
abeyuyajp <yellowmonkey0618@gmail.com>
2021-06-03 18:42:13 +0900

    セッション機能実装の準備

---
+++ backend/app/Post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'work_type',
+    ];
+
+    protected $table = 'posts';
+
+    public function user() {
+        return $this->belongsTo(\App\User::class, 'user_id');
+    }
+}
